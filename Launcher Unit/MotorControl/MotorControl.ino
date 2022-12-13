f6b20331ed23bd3a01c406ae28a8e52a1064dce8
Hassaan <hassaannae@gmail.com>
2022-12-13 22:41:20 +0300

    code to manually control dc motors for topsin, backspin added

---
+++ Launcher Unit/MotorControl/MotorControl.ino
@@ -0,0 +1,123 @@
+// pin Numbers (connect en- to PWM pins, others to any Digitals pins)
+// Motor Top
+int enTop = ;
+int pTop = ;
+int nTop = ;
+
+// Motor Bottom
+int enBottom = ;
+int pBottom = ;
+int nBottom = ;
+
+// Motor Left
+int enLeft = ;
+int pLeft = ;
+int nLeft = ;
+
+// Motor Right
+int enRight = ;
+int pRight = ;
+int nRight = ;
+
+// fast medium slow definitions
+int fast = 240;
+int medium = 180;
+int slow = 120;
+
+
+void setup() {
+  pinMode(enTop, OUTPUT);
+  pinMode(pTop, OUTPUT);
+  pinMode(nTop, OUTPUT);
+  pinMode(enBottom, OUTPUT);
+  pinMode(pBottom, OUTPUT);
+  pinMode(nBottom, OUTPUT);
+  pinMode(enLeft, OUTPUT);
+  pinMode(pLeft, OUTPUT);
+  pinMode(nLeft, OUTPUT);
+  pinMode(enRight, OUTPUT);
+  pinMode(pRight, OUTPUT);
+  pinMode(nRight, OUTPUT);
+
+}
+
+void NoSpin(int speed) {
+  // Turn the motors ON
+  // Top CW
+  digitalWrite(pTop, LOW); 
+  digitalWrite(nTop, HIGH);
+  analogWrite(enTop, speed);
+  
+  // Bottom C
+  digitalWrite(pBottom, HIGH);
+  digitalWrite(nBottom, LOW);
+  analogWrite(enBottom, speed);
+
+  // Left C
+  digitalWrite(pLeft, HIGH);
+  digitalWrite(nLeft, LOW);
+  analogWrite(enLeft, speed);
+
+  // Right CW
+  digitalWrite(pRight, LOW);
+  digitalWrite(nRight, HIGH);
+  analogWrite(enRight, speed);
+}
+
+void Topspin(int speed) {
+  // Turn the motors ON
+  // Top CW
+  digitalWrite(pTop, LOW);
+  digitalWrite(nTop, HIGH);
+  analogWrite(enTop, speed);
+  
+  // Bottom CW
+  digitalWrite(pBottom, LOW);
+  digitalWrite(nBottom, HIGH);
+  analogWrite(enBottom, speed);
+
+  // Left C
+  digitalWrite(pLeft, HIGH);
+  digitalWrite(nLeft, LOW);
+  analogWrite(enLeft, speed);
+
+  // Right CW
+  digitalWrite(pRight, LOW);
+  digitalWrite(nRight, HIGH);
+  analogWrite(enRight, speed);
+}
+
+void Backspin(int speed) {
+  // Turn the motors ON
+  // Top C
+  digitalWrite(pTop, HIGH);
+  digitalWrite(nTop, LOW);
+  analogWrite(enTop, speed);
+  
+  // Bottom C
+  digitalWrite(pBottom, HIGH);
+  digitalWrite(nBottom, LOW);
+  analogWrite(enBottom, speed);
+
+  // Left CW
+  digitalWrite(pLeft, HIGH);
+  digitalWrite(nLeft, LOW);
+  analogWrite(enLeft, speed);
+
+  // Right CW
+  digitalWrite(pRight, LOW);
+  digitalWrite(nRight, HIGH);
+  analogWrite(enRight, speed);
+}
+
+void loop() {
+
+  // Uncomment below to run specific function. Change function argument for slow, medium, fast.
+  
+  NoSpin(medium);
+  
+  // Topspin(medium);
+  
+  // Backspin(medium);
+
+}
